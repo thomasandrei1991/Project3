0f251e28934d8cb8ab1d725b9fff34e25636acd8
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-29 16:27:35 +0800

    All is updated

--- index2.php
+++ index2.php
@@ -5,6 +5,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>
+    <?php
+            if ($firstname == "George" && $lastname == "Alarcon") {
+                echo "<br> Pogi";
+            }
+            else if($firstname == "Maria" && $lastname == "Clara"){
+                echo "<br> Maganda";
+            }
+            else {
+                echo "<br> Walang Pogi or Maganda";
+            }
+        ?>     
+    </h2>
 </body>
 </html>
