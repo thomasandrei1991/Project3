abe1127c0fccf089ec574f3f59ad88ef493c93f1
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-29 11:36:09 +0800

    index1.php updated

--- index1.php
+++ index1.php
@@ -1,16 +1,23 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index1.php" method="post">
-        <label for="firstname">Firstname : </label>
-        <input type="text" id="firstname" name="firstname"><br>
-        <label for="lastname">Lastname : </label>
-        <input type="text" id="lastname" name="lastname"><br>
-        <input type="submit" value="Submit">    
-    </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="index1.php" method="post">
+            <label for="firstname">Firstname : </label>
+            <input type="text" id="firstname" name="firstname"><br>
+            <label for="lastname">Lastname : </label>
+            <input type="text" id="lastname" name="lastname"><br>
+            <input type="submit" value="Submit">    
+            </form>
+    </body>
 </html>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $lastname = htmlspecialchars($_POST['lastname']);
+        echo "Name : " . $firstname . "<br>" . "Lastname : " . $lastname;
+    }
+?>
