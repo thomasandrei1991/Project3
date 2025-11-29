bad57c3397c46c7a218897cf5234a0c8af89be43
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-29 15:30:39 +0800

    Modified index1

--- index1.php
+++ index1.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style1.css">
         <title>Document</title>
     </head>
     <body>
@@ -11,13 +12,28 @@
             <label for="lastname">Lastname : </label>
             <input type="text" id="lastname" name="lastname"><br>
             <input type="submit" value="Submit">    
-            </form>
+        </form>
     </body>
 </html>
-<?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $firstname = htmlspecialchars($_POST['firstname']);
-        $lastname = htmlspecialchars($_POST['lastname']);
-        echo "Name : " . $firstname . "<br>" . "Lastname : " . $lastname;
-    }
-?>
+<h2>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $firstname = htmlspecialchars($_POST['firstname']);
+            $lastname = htmlspecialchars($_POST['lastname']);
+            echo "Name : " . $firstname . "<br>" . "Lastname : " . $lastname;
+        }
+    ?>
+</h2>
+<h2>
+   <?php
+        if ($firstname == "George" && $lastname == "Alarcon") {
+            echo "<br> Pogi";
+        }
+        else if($firstname == "Maria" && $lastname == "Clara"){
+            echo "<br> Maganda";
+        }
+        else {
+            echo "<br> Walang Pogi or Maganda";
+        }
+    ?>     
+</h2>
