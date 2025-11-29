e34298422f32566ccd7bad11e1082efd0ce59cdd
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-29 18:50:11 +0800

    All sets

---
+++ IncludeTutorial/locations.php
@@ -0,0 +1,17 @@
+<?php
+    include 'header.html';
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        You are in Locations Page
+        <br><br>
+    </body>
+</html>
+<?php
+    include 'footer.html';
+?>
--- index2.php
+++ index2.php
@@ -1,22 +1,22 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>
-    <?php
-            if ($firstname == "George" && $lastname == "Alarcon") {
-                echo "<br> Pogi";
-            }
-            else if($firstname == "Maria" && $lastname == "Clara"){
-                echo "<br> Maganda";
-            }
-            else {
-                echo "<br> Walang Pogi or Maganda";
-            }
-        ?>     
-    </h2>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>
+        <?php
+                if ($firstname == "George" && $lastname == "Alarcon") {
+                    echo "<br> Pogi";
+                }
+                else if($firstname == "Maria" && $lastname == "Clara"){
+                    echo "<br> Maganda";
+                }
+                else {
+                    echo "<br> Walang Pogi or Maganda";
+                }
+            ?>     
+        </h2>
+    </body>
 </html>
