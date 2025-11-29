c9a10ad8c5de0859d836c65fe9aebe435a614760
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-29 11:26:47 +0800

    Adding elements on index1.php

--- index1.php
+++ index1.php
@@ -5,6 +5,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="index1.php" method="post">
+        <label for="firstname">Firstname : </label>
+        <input type="text" id="firstname" name="firstname"><br>
+        <label for="lastname">Lastname : </label>
+        <input type="text" id="lastname" name="lastname"><br>
+        <input type="submit" value="Submit">    
+    </form>
 </body>
 </html>
