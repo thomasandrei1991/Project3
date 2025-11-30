b2307bdff692002eb8093bf540297ce482e300cd
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-30 21:05:34 +0800

    All set

---
+++ 7-11/main.php
@@ -0,0 +1,32 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="receiptstyle.css ">
+        <title>7-11 Receipt</title>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <img src="images/7-11 Logo.png" alt="7-11 Logo" class="logo">
+                <p>
+                    Philippine Seven Eleven Corporation <br>
+                    Owned & Operated by: Philippine Seven Corporation <br>
+                    VATREGTIN #000-390-189-0727 <br>
+                    Aragon Bldg 2012 CRM Ave Cor. <br>
+                    Cardinal St. BF ALmanza, Las Pinas City <br>
+                    Tel# : (02) 8778-7111 <br><br><br><br>
+                    11/30/2025 02:15 PM <br><br>
+                    INVOICE NO: 000123456789 <br>
+                    
+                </p>
+                <p></p>
+
+            </header>
+        </div>  
+    </body>
+</html>
+
+<?php
+
+?>
