cb1f1b0199e4cdab2cbd20622b69679c0bee822a
Thomas Andrei <thomasandrei1991@gmail.com>
2025-11-30 22:18:49 +0800

    All is updated

---
+++ 7-11/images/transaction.php
@@ -0,0 +1,17 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="forma-container">
+            <form action="transaction.php">
+                <label for="order">Enter order : </label>
+            </form>
+        </div>
+    </body>
+</html>
+<?php
+    include("main.php");
+?>
--- 7-11/main.php
+++ 7-11/main.php
@@ -8,21 +8,45 @@
     <body>
         <div class="container">
             <header>
-                <img src="images/7-11 Logo.png" alt="7-11 Logo" class="logo">
-                <p>
-                    Philippine Seven Eleven Corporation <br>
-                    Owned & Operated by: Philippine Seven Corporation <br>
-                    VATREGTIN #000-390-189-0727 <br>
-                    Aragon Bldg 2012 CRM Ave Cor. <br>
-                    Cardinal St. BF ALmanza, Las Pinas City <br>
-                    Tel# : (02) 8778-7111 <br><br><br><br>
-                    11/30/2025 02:15 PM <br><br>
-                    INVOICE NO: 000123456789 <br>
-                    
-                </p>
-                <p></p>
-
+                <img src="images/7-11 Logo.png" alt="7-11 Logo" class="logo"><br><br>           
             </header>
+            <section>
+                <div class="container-margin">
+                    <div class="paragraph1-container">
+                        <p id="box1">
+                            Philippine Seven Eleven Corporation <br>
+                            Owned & Operated by: Philippine Seven Corporation <br>
+                            VATREGTIN #000-390-189-0727 <br>
+                            Aragon Bldg 2012 CRM Ave Cor. <br>
+                            Cardinal St. BF ALmanza, Las Pinas City <br>
+                            Tel# : (02) 8778-7111 <br><br><br><br>
+                            11/30/2025 02:15 PM <br><br>
+                            INVOICE NO: 000123456789 <br>
+                        </p>
+                    </div>
+                    <div class="paragraph2-container">
+                        <p id="box2">
+                            Philippine Seven Eleven Corporation <br>
+                            Owned & Operated by: Philippine Seven Corporation <br>
+                            VATREGTIN #000-390-189-0727 <br>
+                            Aragon Bldg 2012 CRM Ave Cor. <br>
+                            Cardinal St. BF ALmanza, Las Pinas City <br>
+                            Tel# : (02) 8778-7111 <br><br><br><br>
+                            11/30/2025 02:15 PM <br><br>
+                            INVOICE NO: 000123456789 <br>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <footer>
+                <div class="paragraph3-container">
+                    <p id="box3">
+                        THIS SERVES AS YOUR OFFICIAL RECEIPT <br>
+                        THANK YOU FOR SHOPPING WITH US! <br>
+                        PLEASE COME AGAIN! <br>
+                    </p>
+                </div>   
+            </footer>
         </div>  
     </body>
 </html>
